52082883313b0b02e20a8c40b7898bef2c29c561
thenriq <>
2023-06-16 18:08:12 +0000

    Implement emailjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+   
+     
+    emailjs.send('service_wuf6p0i', 'template_m9zjylh', {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request":contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+    
+    return false;
+}
